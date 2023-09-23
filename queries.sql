5300664abc981a5a1e159afd40265b2424328a55
waelelsafty07 <waelelsafty07@gmail.com>
2023-09-23 15:39:42 +0300

    fix data & queries

--- queries.sql
+++ queries.sql
@@ -1,77 +1,114 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
-SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
-SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered = TRUE;
-SELECT * FROM animals WHERE name <> 'Gabumon';
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+/* all animals whose name ends in "mon" */
+SELECT * FROM animals 
+WHERE name LIKE '%mon';
 
-BEGIN;
+/* name of all animals born between 2016 and 2019.*/
+SELECT name FROM animals 
+WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
-UPDATE animals SET species = 'Unspecified';
+/* name of all animals that are neutered and have less than 3 escape attempts.*/
+SELECT name FROM animals 
+WHERE neutered = true AND escape_attempts < 3;
 
-SELECT * FROM animals;
+/* date of birth of all animals named either "Agumon" or "Pikachu".*/
+SELECT date_of_birth FROM animals 
+WHERE name IN ('Agumon', 'Pikachu');
 
+/* name and escape attempts of animals that weigh more than 10.5kg*/
+SELECT name, escape_attempts FROM animals 
+WHERE weight_kg > 10.5;
+
+/* all animals that are neutered.*/
+SELECT * FROM animals 
+WHERE neutered = true;
+
+/* all animals not named Gabumon*/
+SELECT * FROM animals 
+WHERE name <> 'Gabumon';
+
+/* all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)*/
+SELECT * FROM animals 
+WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/*Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.*/
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals WHERE species = 'unspecified';
 ROLLBACK;
-
 SELECT * FROM animals;
 
-BEGIN ;
+/*Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.*/
+BEGIN TRANSACTION;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
 
-UPDATE animals SET species = 'Digimon' WHERE name LIKE '%mon';
+/*Update the animals table by setting the species column to pokemon for all animals that don't have species already set.*/
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL OR species = ' ';
 
-UPDATE animals SET species = 'Pokemon' WHERE species IS NULL;
-
-SELECT * FROM animals;
-
+/*commit transaction*/
 COMMIT;
 
-SELECT * FROM animals;
-
-BEGIN ;
-
+/*Inside a transaction delete all records in the animals table, then roll back*/
+BEGIN TRANSACTION;
 DELETE FROM animals;
-
-SELECT * FROM animals;
-
 ROLLBACK;
 
+/*Verify changes*/
 SELECT * FROM animals;
 
-BEGIN ;
+/*beigin transaction*/
 
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+BEGIN TRANSACTION;
+/*Delete all animals born after Jan 1st, 2022*/
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
 
+-- Create a savepoint for the transaction
 SAVEPOINT my_savepoint;
 
-UPDATE animals SET weight_kg = weight_kg * -1;
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals
+SET  weight_kg =  weight_kg * -1;
 
-ROLLBACK TO SAVEPOINT my_savepoint;
+-- Rollback to the savepoint
+ROLLBACK TO my_savepoint;
 
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals
+SET  weight_kg =  weight_kg * -1
+WHERE weight_kg < 0;
 
+-- commit transaction
 COMMIT;
 
-SELECT COUNT(*) AS total_animals FROM animals;
-SELECT COUNT(*) AS animals_never_escaped FROM animals WHERE escape_attempts = 0;
+-- How many animals are there
+SELECT COUNT(*) FROM animals;
 
-SELECT AVG(weight_kg) AS average_weight FROM animals;
+-- How many animals have never tried to escape
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 
-SELECT neutered, SUM(escape_attempts) AS total_escape_attempts
+-- What is the average weight of animals
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals
+SELECT neutered, SUM(escape_attempts) AS total_escapes
 FROM animals
 GROUP BY neutered
-ORDER BY total_escape_attempts DESC
+ORDER BY total_escapes DESC
 LIMIT 1;
 
-SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+-- What is the minimum and maximum weight of each type of animal
+SELECT  species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
 FROM animals
-GROUP BY species;
+GROUP BY  species;
 
-SELECT species, AVG(escape_attempts) AS average_escape_attempts
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
 FROM animals
-WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+WHERE  date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
